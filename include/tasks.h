43d0fe11891b7f52c38a8fc5af577f61e79d2ce8
Nikolay Kochetov <90146288+esthete014@users.noreply.github.com>
2024-03-10 22:01:30 +0300

    Create tasks.h

---
+++ include/tasks.h
@@ -0,0 +1,16 @@
+//
+// Copyright 2024 Kochetov Nikolay
+//
+
+
+#ifndef INCLUDE_TASKS_H_
+#define INCLUDE_TASKS_H_
+
+double RopeAroundPlanetTask(double planetRadius, double radiusIncrement);
+double PathAroundPoolAndFencePriceTask(
+        double poolRadius,
+        double pathWidth,
+        double concretePrice,
+        double fencePrice);
+
+#endif //INCLUDE_TASKS_H_
