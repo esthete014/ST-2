6cdb6de93ea7acda95f3150f75b3ac396dd069fa
Nikolay Kochetov <90146288+esthete014@users.noreply.github.com>
2024-03-10 22:00:43 +0300

    Update circle.h

--- include/circle.h
+++ include/circle.h
@@ -1,7 +1,31 @@
-// Copyright 2022 UNN-CS
+// Copyright 2024 Kochetov Nikolay
 #ifndef INCLUDE_CIRCLE_H_
 #define INCLUDE_CIRCLE_H_
 #include <cstdint>
 
 
 #endif  // INCLUDE_CIRCLE_H_
+#include <string>
+
+class Circle {
+ private:
+    double _radius;
+    double _area;
+    double _ference;
+
+ public:
+    explicit Circle(double radius);
+
+    [[nodiscard]] double GetRadius() const;
+    [[nodiscard]] double GetArea() const;
+    [[nodiscard]] double GetFerence() const;
+
+    void SetRadius(double radius);
+    void SetArea(double area);
+    void SetFerence(double ference);
+
+ private:
+    static void ThrowErrorIfValueIsNotPositive(
+            double value,
+            const std::string& nameOfVariable);
+};
